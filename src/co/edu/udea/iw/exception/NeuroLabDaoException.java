7aff3486041c5515c08e9eacc8e003321feb193b
Juan Guillermo Restrepo Pineda <juangui.rp@gmail.com>
2016-10-17 13:04:02 -0500

    Se hace una primera configuracion de Log4j

--- src/co/edu/udea/iw/exception/NeuroLabDaoException.java
+++ src/co/edu/udea/iw/exception/NeuroLabDaoException.java
@@ -1,16 +1,20 @@
 package co.edu.udea.iw.exception;
 
+import org.apache.log4j.Logger;
+
 public class NeuroLabDaoException extends Exception{
 
+	final static Logger LOGGER = Logger.getLogger(NeuroLabDaoException.class);
 	
 	public NeuroLabDaoException(String message, Throwable cause) {
 		super(message, cause);
-		// TODO Auto-generated constructor stub
+		LOGGER.error("*** Error de conexion: " + message);
+		// LOGGER.error("*** Causa: " + cause);
 	}
 
 	public NeuroLabDaoException(String message) {
 		super(message);
-		// TODO Auto-generated constructor stub
+		LOGGER.error("*** Error de conexion: " + message);
 	}
 
 	public NeuroLabDaoException(Throwable cause) {
