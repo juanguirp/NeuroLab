6a545be4d758a0f21c7f7b8b6a6524fd47f29f54
Juan Guillermo Restrepo Pineda <juangui.rp@gmail.com>
2016-10-17 11:20:20 -0500

    Se crea la primera version del DTO para la tabla tab_tipos_dispositivos

---
+++ src/co/edu/udea/iw/dto/TipoDispositivo.java
@@ -0,0 +1,45 @@
+package co.edu.udea.iw.dto;
+
+/**
+ * Clase que se utilizara como objeto de transferencia de datos para la tabla tab_tipo_dispositivo.
+ * @author Juan Guillermo Restrepo Pineda <juan.restrepo48@udea.edu.co>
+ */
+public class TipoDispositivo {
+	
+	/* Variables de instancia global que representan las
+	 * caracteristicas o atributos que posee un tipo de dispositivo.
+	 */
+	private String id;
+	private String clase;
+	
+	/**
+	 * Constructor para un objeto de la clase TipoDispositivo.
+	 * @param id - Codigo que identifica un tipo de dispositivo.
+	 * @param clase - Nombre de la clase o categoria a la que pertenece un tipo de dispositivo.
+	 */
+	public TipoDispositivo(String id, String clase) {
+		super();
+		this.id = id;
+		this.clase = clase;
+	}
+	
+	/* 
+	 * Getters y Setters para los atributos de un tipo de dispositivo.
+	 */
+	public String getId() {
+		return id;
+	}
+	
+	public void setId(String id) {
+		this.id = id;
+	}
+	
+	public String getClase() {
+		return clase;
+	}
+	
+	public void setClase(String clase) {
+		this.clase = clase;
+	}
+	
+}
