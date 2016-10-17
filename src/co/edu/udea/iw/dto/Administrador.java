fdd02ea452e8dc1d1e52e38c937ca879228e2484
Juan Guillermo Restrepo Pineda <juangui.rp@gmail.com>
2016-10-17 14:38:32 -0500

    Refactor: Se especializan las clases Administrador e Investigador

--- src/co/edu/udea/iw/dto/Administrador.java
+++ src/co/edu/udea/iw/dto/Administrador.java
@@ -4,10 +4,19 @@ package co.edu.udea.iw.dto;
  * Clase que se utilizara como objeto de transferencia de datos para la tabla tab_administradores.
  * @author Juan Guillermo Restrepo Pineda <juan.restrepo48@udea.edu.co>
  */
-public class Administrador extends Usuario {
+public class Administrador {
+	
+	/* Variables de instancia global que representan las
+	 * caracteristicas o atributos que posee un administrador.
+	 */
+	private String id;
+	private String nombre;
+	private String apellidos;
+	private String correo;
+	private String contrasena;
 
 	/**
-	 * Constructor para la clase Administrador heredado de la clase abstracta Usuario. 
+	 * Constructor para la clase Administrador. 
 	 * @param id - Identificador, login o nombre de usuario para un administrador.
 	 * @param nombre - Nombre real de un administrador.
 	 * @param apellidos - Apellidos de un administrador.
@@ -15,6 +24,55 @@ public class Administrador extends Usuario {
 	 * @param contrasena - Contrasena de un administrador.
 	 */
 	public Administrador(String id, String nombre, String apellidos, String correo, String contrasena) {
-		super(id, nombre, apellidos, correo, contrasena);
+		this.id = id;
+		this.nombre = nombre;
+		this.apellidos = apellidos;
+		this.correo = correo;
+		this.contrasena = contrasena;
 	}
+	
+	/* 
+	 * Getters y Setters para los atributos de un administrador.
+	 */
+	
+	public String getId() {
+		return id;
+	}
+	
+	public void setId(String id) {
+		this.id = id;
+	}
+	
+	public String getNombre() {
+		return nombre;
+	}
+	
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
+	public String getApellidos() {
+		return apellidos;
+	}
+	
+	public void setApellidos(String apellidos) {
+		this.apellidos = apellidos;
+	}
+	
+	public String getCorreo() {
+		return correo;
+	}
+	
+	public void setCorreo(String correo) {
+		this.correo = correo;
+	}
+	
+	public String getContrasena() {
+		return contrasena;
+	}
+	
+	public void setContrasena(String contrasena) {
+		this.contrasena = contrasena;
+	}
+	
 }
--- src/co/edu/udea/iw/dto/Investigador.java
+++ src/co/edu/udea/iw/dto/Investigador.java
@@ -4,10 +4,19 @@ package co.edu.udea.iw.dto;
  * Clase que se utilizara como objeto de transferencia de datos para la tabla tab_investigadores.
  * @author Juan Guillermo Restrepo Pineda <juan.restrepo48@udea.edu.co>
  */
-public class Investigador extends Usuario{
+public class Investigador{
 
+	/* Variables de instancia global que representan las
+	 * caracteristicas o atributos que posee un investigador.
+	 */
+	private String id;
+	private String nombre;
+	private String apellidos;
+	private String correo;
+	private String contrasena;
+	
 	/**
-	 * Constructor para la clase Investigador heredado de la clase abstracta Usuario. 
+	 * Constructor para la clase Investigador. 
 	 * @param id - Identificador, login o nombre de usuario para un investigador.
 	 * @param nombre - Nombre real de un investigador.
 	 * @param apellidos - Apellidos de un investigador.
@@ -15,6 +24,55 @@ public class Investigador extends Usuario{
 	 * @param contrasena - Contrasena de un investigador.
 	 */
 	public Investigador(String id, String nombre, String apellidos, String correo, String contrasena) {
-		super(id, nombre, apellidos, correo, contrasena);
+		this.id = id;
+		this.nombre = nombre;
+		this.apellidos = apellidos;
+		this.correo = correo;
+		this.contrasena = contrasena;
 	}	
+	
+	/* 
+	 * Getters y Setters para los atributos de un administrador.
+	 */
+	
+	public String getId() {
+		return id;
+	}
+	
+	public void setId(String id) {
+		this.id = id;
+	}
+	
+	public String getNombre() {
+		return nombre;
+	}
+	
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
+	public String getApellidos() {
+		return apellidos;
+	}
+	
+	public void setApellidos(String apellidos) {
+		this.apellidos = apellidos;
+	}
+	
+	public String getCorreo() {
+		return correo;
+	}
+	
+	public void setCorreo(String correo) {
+		this.correo = correo;
+	}
+	
+	public String getContrasena() {
+		return contrasena;
+	}
+	
+	public void setContrasena(String contrasena) {
+		this.contrasena = contrasena;
+	}
+	
 }
