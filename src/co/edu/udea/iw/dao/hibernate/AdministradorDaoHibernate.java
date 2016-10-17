7b77d2bd1f7ea0eee96017b118f15afd15ba93ec
Juan Guillermo Restrepo Pineda <juangui.rp@gmail.com>
2016-10-17 16:07:07 -0500

    Se implementan los metodos para obtener, guardar, actualizar y eliminar administradores de la base de datos

--- src/co/edu/udea/iw/dao/hibernate/AdministradorDaoHibernate.java
+++ src/co/edu/udea/iw/dao/hibernate/AdministradorDaoHibernate.java
@@ -3,6 +3,9 @@ package co.edu.udea.iw.dao.hibernate;
 import java.util.List;
 
 import org.hibernate.SessionFactory;
+import org.hibernate.criterion.Restrictions;
+import org.hibernate.Criteria;
+import org.hibernate.HibernateException;
 import org.hibernate.Session;
 
 import co.edu.udea.iw.dao.AdministradorDao;
@@ -23,31 +26,111 @@ public class AdministradorDaoHibernate implements AdministradorDao {
 
 	@Override
 	public List<Administrador> listarAdministradores() throws NeuroLabDaoException {
-		// TODO Auto-generated method stub
-		return null;
+		Session session = null;
+		List<Administrador> administradores = null;
+		
+		try {
+			session = sessionFactory.openSession();
+			Criteria criteria = session.createCriteria(Administrador.class);
+			administradores = criteria.list();
+		} catch (HibernateException e) {
+			throw new NeuroLabDaoException(e);
+		}
+		/*
+		 * TODO: Analizar el cierre de la sesion:
+		finally {
+			if(session != null){
+				session.close();
+			}
+		}
+		*/
+		return administradores;
 	}
 
 	@Override
 	public Administrador obtenerAdministrador(String id) throws NeuroLabDaoException {
-		// TODO Auto-generated method stub
-		return null;
+		Session session = null;
+		Administrador administrador = null;
+		
+		try {
+			session = sessionFactory.openSession();
+			Criteria criteria = session.createCriteria(Administrador.class).add(Restrictions.eq("adm_id", id));
+			administrador = (Administrador) criteria.uniqueResult();
+		} catch (HibernateException e) {
+			throw new NeuroLabDaoException(e);
+		}
+		/*
+		 * TODO: Analizar el cierre de la sesion:
+		finally {
+			if(session != null){
+				session.close();
+			}
+		}
+		*/
+		return administrador;
 	}
 
 	@Override
 	public void registrarAdministrador(Administrador administrador) throws NeuroLabDaoException {
-		// TODO Auto-generated method stub
-
+		Session session = null;
+		
+		try {
+			session = sessionFactory.openSession();
+			session.save(administrador);
+		} catch (HibernateException e) {
+			throw new NeuroLabDaoException(e);
+		}
+		/*
+		 * TODO: Analizar el cierre de la sesion:
+		finally {
+			if(session != null){
+				session.close();
+			}
+		}
+		*/
 	}
 
 	@Override
 	public void actualizarAdministrador(Administrador administrador) throws NeuroLabDaoException {
-		// TODO Auto-generated method stub
+		Session session = null;
+		
+		try {
+			session = sessionFactory.openSession();
+			session.update(administrador);
+		} catch (HibernateException e) {
+			throw new NeuroLabDaoException(e);
+		}
+		/*
+		 * TODO: Analizar el cierre de la sesion:
+		finally {
+			if(session != null){
+				session.close();
+			}
+		}
+		*/
 
 	}
 
 	@Override
 	public void eliminarAdministrador(String id) throws NeuroLabDaoException {
-		// TODO Auto-generated method stub
+		Session session = null;
+		Administrador administrador = new Administrador(id, "", "", "", "");
+		
+		try {
+			session = sessionFactory.openSession();
+			session.delete(administrador);
+			
+		} catch (HibernateException e) {
+			throw new NeuroLabDaoException(e);
+		}
+		/*
+		 * TODO: Analizar el cierre de la sesion:
+		finally {
+			if(session != null){
+				session.close();
+			}
+		}
+		*/
 
 	}
 
