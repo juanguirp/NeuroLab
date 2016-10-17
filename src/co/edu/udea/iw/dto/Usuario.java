6866384927b1a2fbdfd20aa7835c2e2b23eb462d
Juan Guillermo Restrepo Pineda <juangui.rp@gmail.com>
2016-10-17 11:19:18 -0500

    Se separan los metodos poniendo un salto de linea entre ellos

--- src/co/edu/udea/iw/dto/Usuario.java
+++ src/co/edu/udea/iw/dto/Usuario.java
@@ -34,33 +34,43 @@ public abstract class Usuario {
 	/* 
 	 * Getters y Setters para los atributos de un tipo de usuario.
 	 */
+	
 	public String getId() {
 		return id;
 	}
+	
 	public void setId(String id) {
 		this.id = id;
 	}
+	
 	public String getNombre() {
 		return nombre;
 	}
+	
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
+	
 	public String getApellidos() {
 		return apellidos;
 	}
+	
 	public void setApellidos(String apellidos) {
 		this.apellidos = apellidos;
 	}
+	
 	public String getCorreo() {
 		return correo;
 	}
+	
 	public void setCorreo(String correo) {
 		this.correo = correo;
 	}
+	
 	public String getContrasena() {
 		return contrasena;
 	}
+	
 	public void setContrasena(String contrasena) {
 		this.contrasena = contrasena;
 	}
