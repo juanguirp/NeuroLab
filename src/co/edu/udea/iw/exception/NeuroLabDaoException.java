b96551714e6c2adcd8b882a47a7f01a972119ddb
Juan Guillermo Restrepo Pineda <juangui.rp@gmail.com>
2016-10-17 12:35:21 -0500

    Se crea una primera version de una excepcion propia que controla los errores de conexion con la base de datos

---
+++ src/co/edu/udea/iw/exception/NeuroLabDaoException.java
@@ -0,0 +1,23 @@
+package co.edu.udea.iw.exception;
+
+public class NeuroLabDaoException extends Exception{
+
+	
+	public NeuroLabDaoException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public NeuroLabDaoException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public NeuroLabDaoException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+
+}
