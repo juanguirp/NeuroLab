afcd0748559ae924da2538c2b643684e04d6e059
Juangui Restrepo <juangui.rp@gmail.com>
2016-10-20 08:17:03 -0500

    Se imprime un log de errores en un archivo

--- src/co/edu/udea/iw/exception/NeuroLabDaoException.java
+++ src/co/edu/udea/iw/exception/NeuroLabDaoException.java
@@ -1,34 +1,30 @@
 package co.edu.udea.iw.exception;
 
-
-import co.edu.udea.iw.test.NeuroLabLogger;
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.Logger;
 
 public class NeuroLabDaoException extends Exception{
 
-	//private NeuroLabLogger LOGGER = NeuroLabLogger.obtenerLogger();
+	static Logger LOGGER = Logger.getLogger(NeuroLabDaoException.class);
 	
 	public NeuroLabDaoException(String message, Throwable cause) {
-		//super(message, cause);
 		
-		
-			//LOGGER.escribirLog("*** Error de conexion: " + message);
-		
-		// LOGGER.error("*** Causa: " + cause);
+		BasicConfigurator.configure();
+		LOGGER.error("*** Mensaje de error: " + message);
+		LOGGER.error("*** Causa: " + cause);
 	}
 
 	public NeuroLabDaoException(String message) {
-		//super(message);
 		
-		
-		//LOGGER.escribirLog("*** Error de conexion: " + message);
+		BasicConfigurator.configure();
+		LOGGER.error("*** Mensaje de error: " + message);
 		
 	}
 
 	public NeuroLabDaoException(Throwable cause) {
-		//super(cause);
 		
-		
-		//LOGGER.escribirLog("*** Error de conexion: " + cause);
+		BasicConfigurator.configure();
+		LOGGER.error("*** Mensaje de error: " + cause);
 		
 	}
 	
