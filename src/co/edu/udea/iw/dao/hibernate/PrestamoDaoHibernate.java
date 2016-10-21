2b5c82984733ffc4f9d2a10a505c53d4484b8bf4
Juangui Restrepo <juangui.rp@gmail.com>
2016-10-21 14:52:31 -0500

    Se crea DAO para Prestamo

---
+++ src/co/edu/udea/iw/dao/hibernate/PrestamoDaoHibernate.java
@@ -0,0 +1,41 @@
+package co.edu.udea.iw.dao.hibernate;
+
+import java.util.List;
+
+import co.edu.udea.iw.dao.PrestamoDao;
+import co.edu.udea.iw.dto.Prestamo;
+import co.edu.udea.iw.exception.NeuroLabDaoException;
+
+public class PrestamoDaoHibernate implements PrestamoDao {
+
+	@Override
+	public List<Prestamo> listarPrestamos() throws NeuroLabDaoException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Prestamo obtenerPrestamo(int id) throws NeuroLabDaoException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void registrarPrestamo(Prestamo prestamo) throws NeuroLabDaoException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void actualizarPrestamo(Prestamo prestamo) throws NeuroLabDaoException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void eliminarPrestamo(String id) throws NeuroLabDaoException {
+		// TODO Auto-generated method stub
+
+	}
+
+}
