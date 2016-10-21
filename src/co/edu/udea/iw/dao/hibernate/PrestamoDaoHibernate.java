defc66257692d9adc542342a42790e96b1b4ca3b
Juangui Restrepo <juangui.rp@gmail.com>
2016-10-21 15:25:00 -0500

    Se implementan los DAOs de SolicitudPrestamo y Prestamo

--- src/co/edu/udea/iw/dao/hibernate/PrestamoDaoHibernate.java
+++ src/co/edu/udea/iw/dao/hibernate/PrestamoDaoHibernate.java
@@ -6,34 +6,86 @@ import co.edu.udea.iw.dao.PrestamoDao;
 import co.edu.udea.iw.dto.Prestamo;
 import co.edu.udea.iw.exception.NeuroLabDaoException;
 
+/**
+ * Implementacion de la interface que permite acceder a la tabla tab_prestamos.
+ * 
+ * @author Juan Guillermo Restrepo Pineda <juan.restrepo48@udea.edu.co>
+ */
 public class PrestamoDaoHibernate implements PrestamoDao {
 
+	/**
+	 * Entrega una lista con los prestamos en la base de datos.
+	 * 
+	 * @return lista de prestamos.
+	 * @throws NeuroLabDaoException
+	 *             cuando hay algun problema en la conexion.
+	 * @see co.edu.udea.iw.dao.PrestamoDao#listarPrestamos()
+	 */
 	@Override
 	public List<Prestamo> listarPrestamos() throws NeuroLabDaoException {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
+	/**
+	 * Entrega los datos de un prestamo dado su identificador.
+	 * 
+	 * @param id
+	 *            - Numero que identifica un prestamo.
+	 * @return DTO de un prestamo con sus datos.
+	 * @throws NeuroLabDaoException
+	 *             cuando hay algun problema en la conexion.
+	 * @see co.edu.udea.iw.dao.PrestamoDao#obtenerPrestamo(int)
+	 */
 	@Override
 	public Prestamo obtenerPrestamo(int id) throws NeuroLabDaoException {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
+	/**
+	 * Permite crear un nuevo prestamo y registrarlo en la base de datos.
+	 * 
+	 * @param -
+	 *            DTO con los datos del administrador a guardar.
+	 * @throws NeuroLabDaoException
+	 *             cuando hay algun problema en la conexion.
+	 * @see co.edu.udea.iw.dao.PrestamoDao#registrarPrestamo(co.edu.udea.iw.dto.
+	 *      Prestamo)
+	 */
 	@Override
 	public void registrarPrestamo(Prestamo prestamo) throws NeuroLabDaoException {
 		// TODO Auto-generated method stub
 
 	}
 
+	/**
+	 * Actualiza la informacion de un prestamo en la base de datos.
+	 * 
+	 * @param prestamo
+	 *            - DTO con los datos del prestamo a modificar.
+	 * @throws NeuroLabDaoException
+	 *             cuando hay algun problema en la conexion.
+	 * @see co.edu.udea.iw.dao.PrestamoDao#actualizarPrestamo(co.edu.udea.iw.dto.
+	 *      Prestamo)
+	 */
 	@Override
 	public void actualizarPrestamo(Prestamo prestamo) throws NeuroLabDaoException {
 		// TODO Auto-generated method stub
 
 	}
 
+	/**
+	 * Permite eliminar un prestamo de la base de datos.
+	 * 
+	 * @param id
+	 *            - Numero que identifica un prestamo.
+	 * @throws NeuroLabDaoException
+	 *             cuando hay algun problema en la conexion.
+	 * @see co.edu.udea.iw.dao.PrestamoDao#eliminarPrestamo(int)
+	 */
 	@Override
-	public void eliminarPrestamo(String id) throws NeuroLabDaoException {
+	public void eliminarPrestamo(int id) throws NeuroLabDaoException {
 		// TODO Auto-generated method stub
 
 	}
--- src/co/edu/udea/iw/dao/hibernate/SolicitudPrestamoDaoHibernate.java
+++ src/co/edu/udea/iw/dao/hibernate/SolicitudPrestamoDaoHibernate.java
@@ -6,32 +6,85 @@ import co.edu.udea.iw.dao.SolicitudPrestamoDao;
 import co.edu.udea.iw.dto.SolicitudPrestamo;
 import co.edu.udea.iw.exception.NeuroLabDaoException;
 
+/**
+ * Implementacion de la interface que permite acceder a la tabla
+ * tab_solicitudes_prestamo.
+ * 
+ * @author Juan Guillermo Restrepo Pineda <juan.restrepo48@udea.edu.co>
+ */
 public class SolicitudPrestamoDaoHibernate implements SolicitudPrestamoDao {
 
+	/**
+	 * Entrega una lista con las solicitudes en la base de datos.
+	 * 
+	 * @return lista de solicitudes.
+	 * @throws NeuroLabDaoException
+	 *             cuando hay algun problema en la conexion.
+	 * @see co.edu.udea.iw.dao.SolicitudPrestamoDao#listarSolicitudes()
+	 */
 	@Override
 	public List<SolicitudPrestamo> listarSolicitudes() throws NeuroLabDaoException {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
+	/**
+	 * Entrega los datos de una solicitud dado su identificador.
+	 * 
+	 * @param id
+	 *            - Numero que identifica una solicitud.
+	 * @return DTO de una solicitud con sus datos.
+	 * @throws NeuroLabDaoException
+	 *             cuando hay algun problema en la conexion.
+	 * @see co.edu.udea.iw.dao.SolicitudPrestamoDao#obtenerSolicitud(int)
+	 */
 	@Override
 	public SolicitudPrestamo obtenerSolicitud(int id) throws NeuroLabDaoException {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
+	/**
+	 * Permite crear una nueva solicitud y registrarla en la base de datos.
+	 * 
+	 * @param solicitudPrestamo
+	 *            - DTO con los datos de la solicitud a guardar.
+	 * @throws NeuroLabDaoException
+	 *             cuando hay algun problema en la conexion.
+	 * @see co.edu.udea.iw.dao.SolicitudPrestamoDao#registrarSolicitud(co.edu.udea.iw
+	 *      .dto.SolicitudPrestamo)
+	 */
 	@Override
 	public void registrarSolicitud(SolicitudPrestamo solicitudPrestamo) throws NeuroLabDaoException {
 		// TODO Auto-generated method stub
 
 	}
 
+	/**
+	 * Actualiza la informacion de una solicitud en la base de datos.
+	 * 
+	 * @param solicitudPrestamo
+	 *            - DTO con los datos de la solicitud a modificar.
+	 * @throws NeuroLabDaoException
+	 *             cuando hay algun problema en la conexion.
+	 * @see co.edu.udea.iw.dao.SolicitudPrestamoDao#actualizarSolicitud(co.edu.udea.
+	 *      iw.dto.SolicitudPrestamo)
+	 */
 	@Override
 	public void actualizarSolicitud(SolicitudPrestamo solicitudPrestamo) throws NeuroLabDaoException {
 		// TODO Auto-generated method stub
 
 	}
 
+	/**
+	 * Permite eliminar una solicitud de la base de datos.
+	 * 
+	 * @param id
+	 *            - Numero que identifica una solicitud.
+	 * @throws NeuroLabDaoException
+	 *             cuando hay algun problema en la conexion.
+	 * @see co.edu.udea.iw.dao.SolicitudPrestamoDao#eliminarSolicitud(int)
+	 */
 	@Override
 	public void eliminarSolicitud(int id) throws NeuroLabDaoException {
 		// TODO Auto-generated method stub
